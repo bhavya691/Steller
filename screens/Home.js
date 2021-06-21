d55523ef9f15d706d0bdbc389b3522b922e408cc
bhavya691 <bhavyarajdeora0@gmail.com>
2021-06-21 22:47:31 +0530

    Stellar-3

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,97 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, ImageBackground, Image } from 'react-native';
+import { View, Text, TouchableOpacity, ImageBackground, Image, Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 
 export default class Home extends React.Component{
     render(){
         return(
-            <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-                <Text>Home Screen</Text>
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidArea} />
+                <ImageBackground source={require('../assets/stars.gif')} style={styles.bgImage}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.titleText}>Stellar</Text>
+                        <Image source={require('../assets/main-icon.png')} style={{height: 300, width: 300}}></Image>
+                    </View>
+
+                    <TouchableOpacity style={styles.routeCard} onPress={() => this.props.navigation.navigate('StarMap')}>
+                        <Text style={styles.routeText}>Star Map</Text>
+                        <Text style={styles.knowMore}>Know More</Text>
+                        <Text style={styles.digit}>1</Text>
+                        <Image source={require('../assets/star_map.png')} style={styles.icon}></Image>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.routeCard} onPress={() => this.props.navigation.navigate('DailyPic')}>
+                        <Text style={styles.routeText}>Daily Pic</Text>
+                        <Text style={styles.knowMore}>Know More</Text>
+                        <Text style={styles.digit}>2</Text>
+                        <Image source={require('../assets/daily_pictures.png')} style={styles.icon}></Image>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.routeCard} onPress={() => this.props.navigation.navigate('SpaceCraft')}>
+                        <Text style={styles.routeText}>Space Craft</Text>
+                        <Text style={styles.knowMore}>Know More</Text>
+                        <Text style={styles.digit}>3</Text>
+                        <Image source={require('../assets/space_crafts.png')} style={styles.icon}></Image>
+                    </TouchableOpacity>
+                </ImageBackground>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    },
+    droidArea:{
+        marginTop: Platform.OS === "android" ? statusbar.currentHeight : 0
+    },
+    bgImage:{
+        resizeMode: 'cover',
+        flex: 1
+    },
+    routeCard:{
+        flex: 0.25,
+        marginLeft: 50,
+        marginTop: 50,
+        marginRight: 50,
+        borderRadius: 30,
+        backgroundColor: '#fff'
+    },
+    routeText:{
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: '#000',
+        marginTop: 75,
+        paddingLeft: 35
+    },
+    knowMore:{
+        color: 'red',
+        fontSize: 16,
+        paddingLeft: 30
+    },
+    digit:{
+        position: 'absolute',
+        fontSize: 100,
+        color: 'rgba(183,183,183,0.6)',
+        right: 20,
+        bottom: -15,
+        zIndex: -1
+    },
+    icon:{
+        height: 150,
+        width: 150,
+        resizeMode: 'contain',
+        left: 1250,
+        top: -150
+    },
+    titleBar:{
+        flex: 0.15,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    titleText:{
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: '#fff'
+    }
+})
--- screens/StarMap.js
+++ screens/StarMap.js
@@ -1,12 +1,74 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, ImageBackground, Image } from 'react-native';
+
+import * as React from 'react';
+import { View, Text, StyleSheet, ImageBackground, Image, TextInput, TouchableOpacity } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default class StarMap extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            latitude: '',
+            longitude: ''
+        }
+    }
+
     render(){
+      const {longitude, latitude} = this.state
+      const path = `https://virtualsky.lco.global/embed/index.html?longitude=${longitude}&latitude=${latitude}&constellations=true&constellationlabels=true&showstarlabels=true&gridlines_az=true&live=true`
         return(
-            <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-                <Text>Star Map Screen</Text>
+            <View style={{flex:1}}>
+                <View style={styles.titleBar}>
+                    <Text style={styles.titleTxt}>Star Map Screen</Text>
+                    <TextInput style={styles.input} placeholder = 'Enter your latitude' placeholderTextColor = '#fff' onChangeText = {(latitude) => {
+                        this.setState({
+                            latitude: latitude
+                        })
+                    }} />
+
+                    <TextInput style={styles.input} placeholder = 'Enter your longitude' placeholderTextColor='#fff' onChangeText = {(longitude) => {
+                        this.setState({
+                            longitude: longitude
+                        })
+                    }} />
+                </View>       
+                <WebView 
+                scalesPageToFit = {true}
+                source = {{uri: path}}
+                style = {{marginTop: -50, marginBottom: -50}}
+                />
             </View>
+            
         )
     }
 }
+
+const styles = StyleSheet.create({
+    input:{
+        borderWidth: 2,
+        borderColor: '#fff',
+        borderRadius: 50,
+        width: '60%',
+        height: '20%',
+        marginTop: 10,
+        display: 'flex',
+        justifyContent: "center",
+        alignItems: "center",
+        paddingLeft: 10,
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 600
+    },
+    titleBar:{
+      backgroundColor: '#1F0024',
+      width: '100%',
+      height: '20%',
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    titleTxt:{
+      color: '#fff',
+      fontSize: 25,
+      fontWeight: "bold",
+    }
+})
